07e5373cf9e04f1985c5730096ddcadc79ddc11b
YADAVSOMURAMYAS <somuuuyadav@gmail.com>
2025-02-08 11:38:56 +0530

    error fixed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,13 +9,14 @@ const {
     sendResetOtp,
     resetPassword
 }= require('../controllers/authController.js');
-const { userAuth } = require('../middlewares/userAuth.js');
+const  userAuth  = require('../middlewares/userAuth.js');
 
 const authRouter = express.Router();
 authRouter.post('/register',register);
 authRouter.post('/login',login);
 authRouter.post('/logout',logout);
 authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp);
+authRouter.post('/verify-account',userAuth,verifyEmail)
 authRouter.post('is-auth',userAuth,isAuthenticated);
 authRouter.post('/send-reset-otp',sendResetOtp);
 authRouter.post('/reset-password',resetPassword);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const userAuth=require('../middlewares/userAuth.js')
-const {getUserData}=require('../controllers/userController.js');
+const getUserData=require('../controllers/userController.js');
 const userRouter=express.Router();
 userRouter.get('/data',userAuth,getUserData);
 module.exports=userRouter;
