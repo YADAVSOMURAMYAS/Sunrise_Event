b39740a0321d983373308bae8b184bdeddb97ac7
YADAVSOMURAMYAS <somuuuyadav@gmail.com>
2025-02-14 22:31:31 +0530

    hb

--- sunrise-event-app/src/pages/Home/Home.jsx
+++ sunrise-event-app/src/pages/Home/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './Home.css';
-import Navbar from '../../components/Navbar/Navbar';
-import About from '../../components/About/About';
-import { AppContent } from '../../context/AppContext';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+import About from '../../components/About/About.jsx';
+import { AppContent } from '../../context/AppContext.jsx';
 import { useContext } from 'react';
 
-import VideoBackground from '../../components/VideoBackground/VideoBackground';
-import MultiEvent from '../../components/multiEvent/MultiEvent';
-import MultiEvent2 from '../../components/MultiEvent/MultiEvent2';
-import MultiEvent3 from '../../components/MultiEvent/MultiEvent3';
-import MultiEvent4 from '../../components/MultiEvent/MultiEvent4';
+import VideoBackground from '../../components/VideoBackground/VideoBackground.jsx';
+import MultiEvent from '../../components/MultiEvent/MultiEvent.jsx';
+import MultiEvent2 from '../../components/MultiEvent/MultiEvent2.jsx';
+import MultiEvent3 from '../../components/MultiEvent/MultiEvent3.jsx';
+import MultiEvent4 from '../../components/MultiEvent/MultiEvent4.jsx';
 
-import Footer from '../../components/Footer/Footer';
-import BookingSection from '../../components/BookingSection/BookingSection';
-import GallerySection from '../../components/GallerySection/GallerySection';
-import ContactSection from '../../components/ContactSection/ContactSection';
+import Footer from '../../components/Footer/Footer.jsx';
+import BookingSection from '../../components/BookingSection/BookingSection.jsx';
+import GallerySection from '../../components/GallerySection/GallerySection.jsx';
+import ContactSection from '../../components/ContactSection/ContactSection.jsx';
 
 import { FaWhatsapp } from 'react-icons/fa'; // Import WhatsApp icon
-import Button from '../../components/Button/Button';
+import Button from '../../components/Button/Button.jsx';
 
 const Home = () => {
   const { userData } = useContext(AppContent);
