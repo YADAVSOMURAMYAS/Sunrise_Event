c5940d4617a5fe71590d6045d9c88c436e1b62e0
YADAVSOMURAMYAS <somuuuyadav@gmail.com>
2025-02-15 01:13:40 +0530

    jdf

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const userModel=require('../models/userModel');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 require('dotenv').config();
