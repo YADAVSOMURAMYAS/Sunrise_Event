b90cd27edc1b2687963c13f70a627c557067cd7a
krajan2006 <kushawahaharikishan1@gmail.com>
2025-02-15 13:17:39 +0530

    service and gallery improved

--- sunrise-event-app/src/pages/Gallery/Gallery.jsx
+++ sunrise-event-app/src/pages/Gallery/Gallery.jsx
@@ -35,7 +35,22 @@ const Gallery = () => {
     "https://s3-us-west-2.amazonaws.com/shaadiwishnewbucket/1e44db08-9374-4c0d-bf64-293b8471feb9.jpg",
     "https://s3-us-west-2.amazonaws.com/shaadiwishnewbucket/2a4426a2-81e7-7586-8c3a-64bb76fdb3ad.jpg",
     "https://s3-us-west-2.amazonaws.com/shaadiwishnewbucket/1973231f-4f2a-1910-c3e5-09d66dcf9e10.jpg",
-    "https://s3-us-west-2.amazonaws.com/shaadiwishnewbucket/95b167a6-67eb-16dd-9716-4752d589d03f.jpg"
+    "https://s3-us-west-2.amazonaws.com/shaadiwishnewbucket/95b167a6-67eb-16dd-9716-4752d589d03f.jpg",
+    "https://i.pinimg.com/originals/da/f0/b5/daf0b56190f59591494732061749c913.jpg",
+    "https://i.pinimg.com/736x/a0/54/9b/a0549b84084a8f6058ec5d7c72e7562e.jpg",
+    "https://deowgxgt4vwfe.cloudfront.net/uploads/1689585920_large.jpg",
+    "https://www.bluelightproductionsllc.com/wp-content/uploads/2020/03/IMG_20140510_165316_748.jpg",
+    "https://cms.interiorcompany.com/wp-content/uploads/2024/01/keep-it-eye-catching-with-balloons-for-republic-day-office-decoration.jpg",
+    "https://i.pinimg.com/originals/ab/e1/aa/abe1aa7cea7e0b1fe547ca11300f6b06.jpg",
+    "https://www.thewowstyle.com/wp-content/uploads/2020/10/Corporate-Event.jpg",
+    "https://www.theknot.com/tk-media/images/3714cc34-eff4-4cc7-b911-86476d6f5646~rs_768.h",
+    "https://i.pinimg.com/originals/bf/20/1a/bf201a5bfca919bd7f80fbcc6f737773.jpg",
+    "https://th.bing.com/th/id/OIP.qkdO8c57FfpiZOLn2ZulZwAAAA?rs=1&pid=ImgDetMain",
+    "https://images.squarespace-cdn.com/content/v1/59cc1d43197aea6d683b9d52/1677102774831-ZU3TB67LFVHBX55S6XYD/macewan_50th_aniverssary-2.jpg",
+    "https://th.bing.com/th/id/OIP.S6xrQoHg49bF9_Vnnw8URQHaLG?rs=1&pid=ImgDetMain",
+    "https://thejarvi.com/wp-content/uploads/2023/01/BIRTH004.jpg",
+    "https://m.media-amazon.com/images/S/aplus-media-library-service-media/dcaea3de-a672-4359-b70a-a735a01313c7.__CR0,0,970,600_PT0_SX970_V1___.jpg",
+    "https://stbonavenue.com/wp-content/uploads/2023/07/3_Blog-07-2023_8-12-Months-Out.jpg",
   ];
   return (
     <div>
