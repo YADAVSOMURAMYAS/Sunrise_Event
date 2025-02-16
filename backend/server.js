019fbbcf2bc7cda47341738668f575083784d0d4
Somu Yadav <152024103+YADAVSOMURAMYAS@users.noreply.github.com>
2025-02-16 23:46:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sunrise-event.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 // Connect to MongoDB
 mongoose
