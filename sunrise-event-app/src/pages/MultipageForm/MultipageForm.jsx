1ffd43226a2b73549cf8cf97569e8a581879807c
YADAVSOMURAMYAS <somuuuyadav@gmail.com>
2025-02-19 01:33:47 +0530

    dsjj

--- sunrise-event-app/src/pages/MultipageForm/MultipageForm.jsx
+++ sunrise-event-app/src/pages/MultipageForm/MultipageForm.jsx
@@ -25,7 +25,7 @@ const MultipageForm = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/events", {
+      const response = await fetch("https://sunrise-event-517y.vercel.app/api/events", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
