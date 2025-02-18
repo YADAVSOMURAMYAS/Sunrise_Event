e9b9aeb6d8132268a27f23ff596074afb4cfffa0
Somu Yadav <152024103+YADAVSOMURAMYAS@users.noreply.github.com>
2025-02-18 23:49:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL,
-    origin:"http://localhost:5174",
+    origin: process.env.FRONTEND_URL,
+    
     credentials: true,
   })
 );
